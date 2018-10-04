dcd68ec7b20c7e0494054063486a8e08243dceed
Davidchhing <davidhchhing@gmail.com>
2018-10-04 16:36:25 -0700

    set up files and folders

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/* eslint no-console: off */
+
+require('dotenv').config();
+const app = require('./lib/app');
+const http = require('http');
+
+const server = http.createServer(app);
+
+const PORT = 9111;
+
+server.listen(PORT, () => {
+    console.log('server running on', server.address().port);
+});
