6eefa12f51f59ed7d3c9da5eb4e7441d2c482bdc
Jack <ajackofsometrades@gmail.com>
2018-10-05 10:15:48 -0700

    initial tests passing

---
+++ test/spies.test.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const Spies = require('../lib/models/Spies');
+
+describe('spies model', () => {
+
+    let createdSpies;
+
+    beforeEach(() => {
+        return Spies.drop();
+    });
+
+    beforeEach(() => {
+        return Promise.all([
+            Spies.create('James Bond', 'PP9', 'Aston Martin'),
+            Spies.create('Jack Ryan', 'Glock 19', 'Bike'),
+            Spies.create('Jason Bourne', 'Fists', 'Feet')
+        ])
+            .then(cs => createdSpies = cs);
+    });
+
+    it('creates a new spy in my db', () => {
+        return Spies.create('Johnny English', 'Incompetence', 'Parachute')
+            .then(createdSpy => {
+                expect(createdSpy).toHaveProperty('_id');
+                expect(createdSpy.name).toEqual('Johnny English');
+                expect(createdSpy.weapon).toEqual('Incompetence');
+                expect(createdSpy.vehicle).toEqual('Parachute');
+            });
+    });
+
+    it('gets a spy by id', () => {
+        return Spies.get(createdSpies[0]._id)
+            .then(receivedSpy => {
+                expect(receivedSpy).toEqual(createdSpies[0]);
+            });
+    });
+});
