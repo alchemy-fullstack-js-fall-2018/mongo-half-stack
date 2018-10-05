330c5380852f8bb4a99933f9e0d1ae6b1c3fa74f
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-05 10:49:51 -0700

    get author by id test red

--- test/authors.test.js
+++ test/authors.test.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 const Authors = require('../lib/models/Authors');
 
 describe('authors model', () => {
+    let _id;
+
     it('adds an author to authors collection', () => {
         return Authors.create('Ursula', 'Le Guin')
             .then(createdAuthor => {
+                _id = createdAuthor._id;
                 expect(createdAuthor).toHaveProperty('_id');
                 expect(createdAuthor.firstName).toEqual('Ursula');
                 expect(createdAuthor.lastName).toEqual('Le Guin');
+            });
+    });
+
+    it('gets an author by id', () => {
+        return Authors.get(_id)
+            .then(receivedAuthor => {
+                expect(receivedAuthor.firstName).toEqual('Ursula');
+                expect(receivedAuthor.lastName).toEqual('Le Guin');
             });
     });
 });
