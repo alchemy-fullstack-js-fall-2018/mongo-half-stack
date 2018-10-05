39fb8543e2e1ea921f4f9ab78615e6892e5fbacc
Davidchhing <davidhchhing@gmail.com>
2018-10-05 10:40:50 -0700

    updated noodle js file with db

---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('noodles and their restaurant', () => {
+
+    it('this creates a noodles location', () => {
+        return request(app).post('/noodles')
+            .send({ name: 'what the Pho', location: 'the bronx' })
+            .then(res => {
+                const json = JSON.parse(res.text);
+                expect(json.id).toEqual(expect.any(String));
+                expect(json.name).toEqual('what the Pho');
+                expect(json.location).toEqual('the bronx');
+            });
+    });
+
+}); 
