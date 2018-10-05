a4b73299b87bb06789adbfea47742e974343730d
KLPainter <KLPainter@gmail.com>
2018-10-05 11:36:13 -0700

    create 3 docs and delete all working

--- test/authors.test.js
+++ test/authors.test.js
@@ -2,23 +2,42 @@ require('dotenv').config();
 const Authors = require('../lib/models/Authors');
 
 describe('authors model', () => {
-    let _id;
+    let createdAuthors;
 
-    it('adds an author to authors collection', () => {
-        return Authors.create('Ursula', 'Le Guin')
-            .then(createdAuthor => {
-                _id = createdAuthor._id;
-                expect(createdAuthor).toHaveProperty('_id');
-                expect(createdAuthor.firstName).toEqual('Ursula');
-                expect(createdAuthor.lastName).toEqual('Le Guin');
+    beforeEach(() => {
+        return Authors.drop();
+    });
+
+    beforeEach(() => {
+        return Promise.all([
+            Authors.create('Ursula', 'Le Guin'),
+            Authors.create('Virginia', 'Woolf'),
+            Authors.create('Toni', 'Morrison')
+        ])
+            .then(createdAuthorsFromPromise => {
+                createdAuthors = createdAuthorsFromPromise;
             });
     });
 
+    it('tests to make sure an author was added', () => {
+        expect(createdAuthors[0]).toHaveProperty('_id');
+        expect(createdAuthors[0].firstName).toEqual('Ursula');
+        expect(createdAuthors[0].lastName).toEqual('Le Guin');
+    });   
+
     it('gets an author by id', () => {
-        return Authors.get(_id)
+        return Authors.get(createdAuthors[0]._id)
             .then(receivedAuthor => {
                 expect(receivedAuthor.firstName).toEqual('Ursula');
                 expect(receivedAuthor.lastName).toEqual('Le Guin');
             });
     });
+
+    // it('gets all authors', () => {
+    //     return Authors.get()
+    //         .then(receivedAuthors => {
+    //             expect(receivedAuthors.length).
+    //         });
+    // });
+
 });
