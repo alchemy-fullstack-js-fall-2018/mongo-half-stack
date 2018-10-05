71f21f0b3d37801b2052d608d8e7906ee6cf8f38
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-04 17:55:39 -0700

    test written and failing for create

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,22 @@
 const request = require('supertest');
+require('dotenv').config();
 const app = require('../lib/app');
 
+describe('dog bird farm for dogs and birds', () => {
+
+    // let createdDogs;
+    // let createdBirds;
+
+    it('creates a new dog in our db', () => {
+        return request(app).post('/dogs')
+            .send('tito', 'pitbull')
+            .then(res => {
+                const json = JSON.parse(res);
+                expect(json).toHaveProperty('_id');
+                expect(json.name).toEqual('tito');
+                expect(json.breed).toEqual('pitbull');
+            });
+    });
+
+
+});
