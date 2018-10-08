103b8c83141073f6b6ca6141b805137afa9cd4f6
KLPainter <KLPainter@gmail.com>
2018-10-08 11:00:51 -0700

    working on beforeEach

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,35 @@
+require('dotenv').config();
 const request = require('supertest');
 const app = require('../lib/app');
-// const Authors = require('../lib/models/Authors');
+const Authors = require('../lib/models/Authors');
 // const Books = require('../lib/models/Books');
 
 describe('app authors', () => {
+
+    // beforeEach(() => {
+    //     return Authors.drop();
+    // });
+
+    // [ { firstName: 'Ursula', lastName: 'Le Guin' };
+    // const newAuthor2 = { firstName: 'Virginia', lastName: 'Woolf' };
+    // const newAuthor3 = { firstName: 'Toni', lastName: 'Morrison' };
+
+    // beforeEach(() => {
+    //     return Promise.all([
+    //        return request(app).post('/authors').send(newAuthor1);
+    //     ])
+    //         .then(createdAuthorsFromPromise => {
+    //             createdAuthors = createdAuthorsFromPromise;
+    //         });
+    // });
+
     it('creates an author', () => {
         const newAuthor = { firstName: 'Ursula', lastName: 'Le Guin' };
         return request(app).post('/authors')
             .send(newAuthor)
             .then(response => {
-                expect(response.body).toHaveProperty('_id');
+                console.log(response.body._id);
+                expect(response.body._id).toEqual(expect.any(String));
                 expect(response.body.firstName).toEqual('Ursula');
                 expect(response.body.lastName).toEqual('Le Guin');
             });
