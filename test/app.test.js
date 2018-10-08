cc834b9ff30527f8abdc730e090caf1495d8a8d4
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-08 10:25:48 -0700

    working on first test using http

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,17 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const Authors = require('../lib/models/Authors');
-const Books = require('../lib/models/Books');
+// const Authors = require('../lib/models/Authors');
+// const Books = require('../lib/models/Books');
 
 describe('app authors', () => {
-    it('gets an author by id', () => {
+    it('creates an author', () => {
         const newAuthor = { firstName: 'Ursula', lastName: 'Le Guin' };
         return request(app).post('/authors')
             .send(newAuthor)
             .then(response => {
-                expect(response.body).toEqual(newAuthor);
+                expect(response.body).toHaveProperty('_id');
+                expect(response.body.firstName).toEqual('Ursula');
+                expect(response.body.lastName).toEqual('Le Guin');
             });
     });
 });
