7c1ceff7b446518efbfd67f762cbbc0c1aa379b2
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-08 11:10:43 -0700

    creates author test green, refactored app.test w/ before each

--- test/app.test.js
+++ test/app.test.js
@@ -5,33 +5,40 @@ const Authors = require('../lib/models/Authors');
 // const Books = require('../lib/models/Books');
 
 describe('app authors', () => {
+    const authors = [
+        { firstName: 'Ursula', lastName: 'Leguin' },
+        { firstName: 'Virginia', lastName: 'Woolf' },
+        { firstName: 'Toni', lastName: 'Morrison' }
+    ];
 
-    // beforeEach(() => {
-    //     return Authors.drop();
-    // });
+    let createdAuthors;
 
-    // [ { firstName: 'Ursula', lastName: 'Le Guin' };
-    // const newAuthor2 = { firstName: 'Virginia', lastName: 'Woolf' };
-    // const newAuthor3 = { firstName: 'Toni', lastName: 'Morrison' };
+    const creator = author => {
+        return request(app).post('/authors')
+            .send(author);
+    };
 
-    // beforeEach(() => {
-    //     return Promise.all([
-    //        return request(app).post('/authors').send(newAuthor1);
-    //     ])
-    //         .then(createdAuthorsFromPromise => {
-    //             createdAuthors = createdAuthorsFromPromise;
-    //         });
-    // });
+    beforeEach(() => {
+        return Authors.drop();
+    });
+
+    beforeEach(() => {
+        return Promise.all(authors.map(creator))
+            .then(authors => {
+                createdAuthors = authors.map(author => author.body);
+            });
+    });
+
 
     it('creates an author', () => {
-        const newAuthor = { firstName: 'Ursula', lastName: 'Le Guin' };
+        const newAuthor = { firstName: 'Stephen', lastName: 'King' };
         return request(app).post('/authors')
             .send(newAuthor)
             .then(response => {
                 console.log(response.body._id);
                 expect(response.body._id).toEqual(expect.any(String));
-                expect(response.body.firstName).toEqual('Ursula');
-                expect(response.body.lastName).toEqual('Le Guin');
+                expect(response.body.firstName).toEqual('Stephen');
+                expect(response.body.lastName).toEqual('King');
             });
     });
 });
